39d0a30a66d5c1847b429e219a747c1f8f89abc4
24f2002727 <shivamrock811307@gmail.com>
2025-12-29 23:08:23 +0530

    Api key removed

--- project5/script.js
+++ project5/script.js
@@ -1,4 +1,4 @@
-const apiKey = "7f784361cec84053497f0936aff89f4a";;
+const apiKey = "apikey";;
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const geoApiUrl = "https://api.openweathermap.org/data/2.5/weather";
 const DEFAULT_CITY = "Delhi";
